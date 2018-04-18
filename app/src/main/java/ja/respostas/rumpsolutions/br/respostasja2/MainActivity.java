31dcc051696b92d53a0e3cfbcf55780bfadf0c59
seila16 <jonathan.oliveira16@yahoo.com.br>
2018-04-18 15:23:36 -0300

    Initial commit

---
+++ app/src/main/java/ja/respostas/rumpsolutions/br/respostasja2/MainActivity.java
@@ -0,0 +1,14 @@
+package ja.respostas.rumpsolutions.br.respostasja2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //testando se vai aparecer ai
+    }
+}
