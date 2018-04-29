c506d33af5d07912510791262e97987741225136
seila16 <jonathan.oliveira16@yahoo.com.br>
2018-04-29 20:52:31 -0300

    ficou muito bom

--- app/src/main/java/ja/respostas/rumpsolutions/br/respostasja2/Aplication/CreatePost.java
+++ app/src/main/java/ja/respostas/rumpsolutions/br/respostasja2/Aplication/CreatePost.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.text.InputFilter;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
@@ -24,9 +26,11 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.security.spec.PSSParameterSpec;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import ja.respostas.rumpsolutions.br.respostasja2.R;
@@ -43,6 +47,11 @@ public class CreatePost extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference userReference;
     private Usuario user;
+    private EditText titulo;
+    private DatabaseReference uid;
+    private List<String> nomesM = new ArrayList<String>();
+    private String aux;
+
 
 
     @Override
@@ -57,6 +66,7 @@ public class CreatePost extends AppCompatActivity {
         usuario = findViewById(R.id.postUsuario);
         SimpleDateFormat dateFormat_hora = new SimpleDateFormat("dd-MM-yyyy-HH:mm");
         materia = findViewById(R.id.postMateria);
+        titulo = findViewById(R.id.postTitle);
 
         databaseReference = FirebaseDatabase.getInstance().getReference();
 
@@ -72,8 +82,55 @@ public class CreatePost extends AppCompatActivity {
                 (cal.get(Calendar.HOUR_OF_DAY)<10 ? "0" : "") + cal.get(Calendar.HOUR_OF_DAY)+":"+
                 (cal.get(Calendar.MINUTE)<10 ? "0" : "") + cal.get(Calendar.MINUTE);
 
+        //instaciando a classe materias e pegando os dados dela, em seguida jogando dentro do arraylist criado nomeM.
 
 
+
+        //criar um arrayadapter usando um padrao de layout da classe R do android passando o arraylist nomesM
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,nomesM);
+
+        ArrayAdapter<String> spinnerArrayAdapter = arrayAdapter;
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        materia.setAdapter(spinnerArrayAdapter);
+
+        //metodo do spinner para capturar o item selecionado
+
+        materia.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //pega o nome pela posição
+                aux = adapterView.getItemAtPosition(i).toString();
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("server/saving-data/respostas-ja/materias");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()){
+                    Materias mater = dataSnapshot.getValue(Materias.class);
+                   while (mater.getNome() != null) {
+                       nomesM.add(mater.getNome());
+                   }
+                }
+
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -82,6 +139,7 @@ public class CreatePost extends AppCompatActivity {
         FirebaseUser currentUser = mAuth.getCurrentUser();
         user = new Usuario(this, currentUser);
         userReference = user.getReference();
+        uid = userReference;
         userReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -114,7 +172,7 @@ public class CreatePost extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.post_post) {
-            writeNewPostagem("1",user.getIdUser(),"Materia Teste","title",conteudo.getText().toString(), hora);
+            writeNewPostagem(uid.toString(),user.getIdUser(),"teste",titulo.getText().toString(),conteudo.getText().toString(), hora);
             funcoes.toast(this,"Pergunta postada");
             return true;
         }
@@ -137,7 +195,7 @@ public class CreatePost extends AppCompatActivity {
         post.put("uid"      , postagem.getUid()         );
         post.put("materia"  , postagem.getMateria()     );
         post.put("hora"     , postagem.getHora()        );
-        post.put("titulo"   , "teste"                   );
+        post.put("titulo"   , postagem.getTitulo()      );
 
         Map<String , Object> childUpdates = new HashMap<>();
         childUpdates.put("/postagens/" + key, post);
